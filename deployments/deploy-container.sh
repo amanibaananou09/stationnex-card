1a840388ca75836678e7239f2ac2289741ffb5a6
Akram MONCER <akram.moncer@gmail.com>
2024-06-20 10:48:43 +0200

    add pipeline files

--- deployments/deploy-container.sh
+++ deployments/deploy-container.sh
@@ -8,4 +8,4 @@ echo 'Delete $DOCKER_REGISTRY_URL/$PROJECT_NAME Image'
 docker images -a | grep ${PROJECT_NAME} | awk '{print $1":"$2}' | xargs docker rmi
 
 echo 'Deploy New Container'
-docker run -e "PORT=$PORT" -d --network ess --name ${PROJECT_NAME} --restart=unless-stopped ${DOCKER_REGISTRY_URL}/${PROJECT_NAME}:${IMAGE_TAG}
+docker run -d --network ess --name ${PROJECT_NAME} --restart=unless-stopped ${DOCKER_REGISTRY_URL}/${PROJECT_NAME}:${IMAGE_TAG}
