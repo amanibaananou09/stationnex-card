9b3107b64b3691622dd0d2b79da60558c7337d35
mansourmanel <mansourmanel7@gmail.com>
2024-06-28 12:20:51 +0100

    [CM-99]Esapce Client : Suivi des transactions effectuées par les cartes

--- src/components/Charts/Fuelvolume.tsx
+++ src/components/Charts/Fuelvolume.tsx
@@ -1,26 +1,84 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactApexChart from "react-apexcharts";
+import { PeriodeProps } from "../../common/react-props";
+import { useAuth } from "../../store/AuthContext";
+import { getChartByFuelCartPeriod } from "../../common/api/chart-api";
 
-const Fuelvolume = () => {
-  const series = [
-    {
-      name: "Gasoil",
-      data: [10, 20, 30],
-    },
-    {
-      name: "Super sans plomb",
-      data: [50, 60, 70],
-    },
-    {
-      name: "Gasoil sans soufre",
-      data: [26, 90, 50],
-    },
-  ];
+const FuelvolumeCart = ({ periode, startDate, endDate }: PeriodeProps) => {
+  const [chartData, setChartData] = useState<{
+    series: any[];
+    categories: string[];
+  }>({ series: [], categories: [] });
+  const { customerId } = useAuth();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getChartByFuelCartPeriod(
+          periode,
+          startDate,
+          endDate,
+          customerId,
+        );
+        processChartData(data);
+      } catch (error) {
+        console.error("Error fetching chart data:", error);
+      }
+    };
+
+    fetchData();
+  }, [periode, startDate, endDate, customerId]);
+
+  const processChartData = (data: any[]) => {
+    const seriesData: { [key: string]: { [key: string]: number } } = {};
+    const categories: Set<string> = new Set();
+
+    // Collect data and cardIdentifiers
+    data.forEach((transaction) => {
+      if (!seriesData[transaction.fuelGrade]) {
+        seriesData[transaction.fuelGrade] = {};
+      }
+      seriesData[transaction.fuelGrade][transaction.cardIdentifier] =
+        transaction.sum;
+      categories.add(transaction.cardIdentifier);
+    });
+
+    const allCategories = Array.from(categories);
+
+    const series = Object.keys(seriesData).map((fuelGrade) => ({
+      name: fuelGrade,
+      data: allCategories.map((cardId) => seriesData[fuelGrade][cardId] || 0),
+    }));
+
+    setChartData({
+      series: series,
+      categories: allCategories,
+    });
+  };
+
   const options: ApexCharts.ApexOptions = {
     chart: {
       type: "bar",
       toolbar: {
         show: false,
+      },
+    },
+    legend: {
+      position: "right",
+      horizontalAlign: "center",
+      fontSize: "14px",
+      fontWeight: 500,
+      fontFamily: "inherit",
+      markers: {
+        width: 8,
+        height: 8,
+      },
+      itemMargin: {
+        horizontal: 15,
+        vertical: 8,
+      },
+      labels: {
+        colors: "black",
       },
     },
     plotOptions: {
@@ -30,8 +88,20 @@ const Fuelvolume = () => {
         borderRadius: 4,
       },
     },
+    colors: [
+      "#008FFB",
+      "#00E396",
+      "#FEB019",
+      "#FF4560",
+      "#775DD0",
+      "#3F51B5",
+      "#546E7A",
+      "#D4526E",
+      "#8D5B4C",
+      "#F86624",
+    ],
     dataLabels: {
-      enabled: false,
+      enabled: true,
     },
     stroke: {
       show: true,
@@ -42,7 +112,13 @@ const Fuelvolume = () => {
       title: {
         text: "IDs des cartes",
       },
-      categories: ["FR01", "FR02", "FR03"],
+      labels: {
+        style: {
+          fontSize: "12px",
+        },
+        offsetY: 0,
+      },
+      categories: chartData.categories,
     },
     yaxis: {
       title: {
@@ -65,7 +141,7 @@ const Fuelvolume = () => {
     <div>
       <ReactApexChart
         options={options}
-        series={series}
+        series={chartData.series}
         type="bar"
         height={450}
       />
@@ -73,4 +149,4 @@ const Fuelvolume = () => {
   );
 };
 
-export default Fuelvolume;
+export default FuelvolumeCart;
---
+++ src/components/Charts/VolumeCard.tsx
@@ -0,0 +1,137 @@
+import React, { useEffect, useState } from "react";
+import ReactApexChart from "react-apexcharts";
+import { PeriodeProps } from "../../common/react-props";
+import { useAuth } from "../../store/AuthContext";
+import { getdailyChart } from "../../common/api/chart-api";
+
+const VolumeCard = ({ periode, startDate, endDate }: PeriodeProps) => {
+  const [series, setSeries] = useState<any[]>([]);
+  const [categories, setCategories] = useState<string[]>([]);
+  const { customerId } = useAuth();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getdailyChart(
+          periode,
+          startDate,
+          endDate,
+          customerId,
+        );
+
+        const chartData: { [key: string]: number[] } = {};
+        const cardIdentifiers: string[] = [];
+
+        data.forEach(
+          (entry: { cardIdentifier: string; sum: number; date: string }) => {
+            if (!cardIdentifiers.includes(entry.cardIdentifier)) {
+              cardIdentifiers.push(entry.cardIdentifier);
+            }
+
+            if (!chartData[entry.date]) {
+              chartData[entry.date] = new Array(cardIdentifiers.length).fill(0);
+            }
+
+            const cardIndex = cardIdentifiers.indexOf(entry.cardIdentifier);
+            chartData[entry.date][cardIndex] = entry.sum;
+          },
+        );
+
+        const seriesData = cardIdentifiers.map((identifier, index) => ({
+          name: `${identifier}`,
+          data: Object.keys(chartData).map((date) => chartData[date][index]),
+        }));
+
+        const dates = Object.keys(chartData);
+
+        setCategories(dates);
+        setSeries(seriesData);
+      } catch (error) {
+        console.error("Error fetching chart data:", error);
+      }
+    };
+
+    fetchData();
+  }, [periode, startDate, endDate, customerId]);
+
+  const options: ApexCharts.ApexOptions = {
+    chart: {
+      type: "line",
+      zoom: {
+        enabled: false,
+      },
+      toolbar: {
+        show: false,
+      },
+    },
+    colors: [
+      "#008FFB",
+      "#00E396",
+      "#FEB019",
+      "#FF4560",
+      "#775DD0",
+      "#3F51B5",
+      "#546E7A",
+      "#D4526E",
+      "#8D5B4C",
+      "#F86624",
+    ],
+    legend: {
+      position: "right",
+      horizontalAlign: "center",
+      fontSize: "14px",
+      fontWeight: 500,
+      fontFamily: "inherit",
+      markers: {
+        width: 8,
+        height: 8,
+      },
+      itemMargin: {
+        horizontal: 15,
+        vertical: 8,
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      show: true,
+      width: 2,
+      curve: "straight",
+    },
+    markers: {
+      size: 5,
+      strokeWidth: 2,
+      hover: {
+        sizeOffset: 2,
+      },
+    },
+    xaxis: {
+      categories: categories,
+    },
+    yaxis: {},
+    fill: {
+      opacity: 1,
+    },
+    tooltip: {
+      y: {
+        formatter: function (val: number) {
+          return `${val}`;
+        },
+      },
+    },
+  };
+
+  return (
+    <div>
+      <ReactApexChart
+        options={options}
+        series={series}
+        type="line"
+        height={450}
+      />
+    </div>
+  );
+};
+
+export default VolumeCard;
