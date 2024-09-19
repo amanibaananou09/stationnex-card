602f145b245dbf859846722dcdc39249857d604f
user <baananouamani090@gmail.com>
2024-09-19 18:15:17 +0100

    update fuelGrade price add pagination to salePoint, user, product fix bug of volume in dashbord

--- src/common/api/configuration-api.ts
+++ src/common/api/configuration-api.ts
@@ -1,15 +1,5 @@
-import {
-  FuelGrade,
-  Pump,
-  Station,
-  TankDelivery,
-  TankDeliveryCreteria,
-  Transaction,
-  TransactionCreteria,
-} from "common/model";
+import { Transaction, TransactionCreteria } from "common/model";
 import api from "./axios";
-
-const API_URL = "/configuration";
 
 export const getAllTransaction = async (
   creteria: TransactionCreteria,
@@ -33,22 +23,6 @@ export const getAllTransaction = async (
         to: creteria.period.to,
       },
     },
-  );
-
-  return response.data;
-};
-
-export const getAllTankDelivery = async (
-  station: Station,
-  creteria: TankDeliveryCreteria,
-): Promise<{
-  content: TankDelivery[];
-  totalPages: number;
-  totalElements: number;
-  numberOfElements: number;
-}> => {
-  const response = await api.get(
-    `${API_URL}/delivery/${station.controllerPts.id}?filterType=${creteria.filterType}&tank=${creteria.tank}&startDate=${creteria.startDate}&endDate=${creteria.endDate}&page=${creteria.page}&size=${creteria.size}`,
   );
 
   return response.data;
